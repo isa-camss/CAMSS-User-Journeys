fd90ef0f97267d22f3a109e9ed05d063b9234d5e
Manuela Grajales Duque <mgrajale@everis.nttdata.com>
2021-11-15 17:17:58 +0100

    integration between API and ipynotebook

--- dev/apis/assessments/camss_assessments.py
+++ dev/apis/assessments/camss_assessments.py
@@ -5,10 +5,9 @@ import cfg.ctt as ctt
 api = Namespace("assessments", description="CAMSS Assessments")
 
 assess_args = api.parser()
-assess_args.add_argument('operation_code', required=True, default='BR-M1113', type=str,
-                      help='Type the operation code of the resource whose wordcloud you want to find. Take into account'
-                           " that if you want to find the wordcloud of a document, "
-                           "you need to type in the resource_id parameter.")
+assess_args.add_argument('sparql_query', required=True, default='', type=str,
+                      help='Type the query to execute an sparql query throught CELLAR triplestore and get information '
+                           'about CAMSS solutions')
 
 
 @api.route('/get_assessments')
@@ -17,11 +16,13 @@ class ApiAssessments(Resource):
     @api.doc(body="Obtains CAMSS assessments resources")
     def post(self):
         """
-        Main entry point to the GSF Thesaurus Re-indexation Pipeline, which produces all artifacts and sets the search up for working with the new thesauri.
+        Main entry point to the GSF Thesaurus Re-indexation Pipeline, which produces all artifacts and sets the search
+        up for working with the new thesauri.
         """
+        args=assess_args.parse_args()
         assess = Assessments(ctt.CELLAR_CONNECTION)
         # t0 = io.now()
-        msg, code = assess.get_assessment()
+        msg = assess.get_assessment(args['sparql_query'])
         # io.log(f'Success: Process concluded. It took {io.now() - t0}')
         # msg['lapse'] = f'{str(io.now() - t0)})'
-        return msg, code
+        return msg, 201
--- dev/com/assessments/assessments.py
+++ dev/com/assessments/assessments.py
@@ -7,14 +7,14 @@ class Assessments:
     def __init__(self, url_connection: str):
         self.url = url_connection
 
-    def get_assessment(self) -> dict:
+    def get_assessment(self, query: str) -> dict:
         # Establish connection to cellar
         cellar_connection = SPARQLWrapper(self.url)
 
         # Execute query
-        cellar_connection.setQuery(sparql.QUERY_ASSESSMENTS)
+        cellar_connection.setQuery(query)
         # Format results
         cellar_connection.setReturnFormat(JSON)
         qres = cellar_connection.query().convert()
 
-        return qres, 201
+        return qres
