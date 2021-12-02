044d261a7d1f7c9ee72d5e108b565105b2335dd8
Manuela Grajales Duque <mgrajale@everis.nttdata.com>
2021-12-02 10:52:03 +0100

    use case 1 and 2 were created

--- dev/cfg/ctt.py
+++ dev/cfg/ctt.py
@@ -6,15 +6,19 @@ CELLAR_CONNECTION = "http://publications.europa.eu/webapi/rdf/sparql"
 URL_MICROSERVICE = 'http://localhost:5100/assessments/get_assessments'
 
 TAB_VALUES = [
-    {'name': "Custom queries", 
-     'query_value': '', 
-     'description': '''In this section you can make a SPARQL query of whatever you want to Cellar Triplestore.br &nbsp; 
-      Type the query in the textarea and press the button to execute it.'''},
     {'name': 'UC1: Specs Catalog', 
      'query_value': sparql.QUERY_ALL_SPECIFICATIONS_ASSESSED, 
      'description':'''This SPARQL query returns the interoperability specifications catalog by CAMSS and their 
       assessments. Press the Run button to execute it. You can also modify the query if you need to complement it''' },
-    {'name': 'UC2: EIRA ABBs', 
-     'query_value': sparql.QUERY_SPECIFICATIONS_ASSESSED_ASSOCIATED_TO_EIRA_ABBS, 
-     'description': '''This SPARQL query, given one or more ABBs get the specifications linked to that ABBs. Press the 
-      Run button to execute it. You can also modify the query if you need to complement it'''}]
+    {'name': 'UC2: One Specification Assessement', 
+     'query_value': '', 
+     'description': '''Re-directs the user to one Specification Assessment dataset and report. Press the Run button to execute it. You can also 
+     modify the query if you need to complement it'''},
+    {'name': 'UC3: Scenario-based Assessement', 
+     'query_value': '', 
+     'description': '''Downloads one concrete scenario-based CAMSS Assessments. ress the Run button to execute it. You can also modify the query 
+     if you need to complement it'''},
+    {'name': "Custom queries", 
+     'query_value': '', 
+     'description': '''In this section you can develop a SPARQL query of your own. Type the query in the textarea and press the button to execute 
+     it.'''}]
