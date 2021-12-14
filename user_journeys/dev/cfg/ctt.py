ef1d27398bd6c772aa290b68112fc26257a2b087
Manuela Grajales Duque <mgrajale@everis.nttdata.com>
2021-12-14 16:47:24 +0100

    user flow structure is defined

--- user_journeys/dev/cfg/ctt.py
+++ user_journeys/dev/cfg/ctt.py
@@ -24,15 +24,15 @@ PROCURER_USE_CASES = [
     {'name': "Custom queries",
      'title': '',
      'query_value': '',
-     'description': '''In this section you can develop a SPARQL query of your own. Type the query in the textarea and press the button to execute 
-     it.'''}]
+     'description': '''In this section you can develop a SPARQL query of your own. Type the query in the textarea and press the Run button to
+     execute it.'''}]
 
 SOFTWARE_USE_CASES = [
     {'name': 'UC-1',
-     'title': 'SPECIFICATIONS CATALOG',
-     'query_value': sparql.QUERY_ALL_SPECIFICATIONS_ASSESSED,
-     'description': '''This SPARQL query returns the interoperability specifications catalog by CAMSS and their 
-      assessments. Press the Run button to execute it. You can also modify the query if you need to complement it'''},
+     'title': 'SPECIFICATIONS ASSOCIATED TO EIRA ABBs',
+     'query_value': sparql.QUERY_SPECIFICATIONS_ASSESSED_ASSOCIATED_TO_EIRA_ABBS,
+     'description': '''This SPARQL query returns the interoperability specifications assessed by CAMSS and associated to EIRA ABBs.
+     Press the Run button to execute it. You can also modify the query if you need to complement it'''},
     {'name': 'UC-2',
      'title': 'ONE SPECIFICATION ASSESSMENT',
      'query_value': '',
@@ -41,10 +41,9 @@ SOFTWARE_USE_CASES = [
     {'name': 'UC-3',
      'title': '',
      'query_value': '',
-     'description': '''Downloads one concrete scenario-based CAMSS Assessments. ress the Run button to execute it. You can also modify the query 
-     if you need to complement it'''},
+     'description': '''...'''},
     {'name': "Custom queries",
      'title': '',
      'query_value': '',
-     'description': '''In this section you can develop a SPARQL query of your own. Type the query in the textarea and press the button to execute 
-     it.'''}]
+     'description': '''In this section you can develop a SPARQL query of your own. Type the query in the textarea and press the Run button to
+     execute it.'''}]
