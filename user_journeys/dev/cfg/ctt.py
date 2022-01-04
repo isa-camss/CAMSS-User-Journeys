b17a60101669cf35cd80e786acd24f1b30ee6190
Manuela Grajales Duque <mgrajale@everis.nttdata.com>
2022-01-04 18:53:07 +0100

    correction of user stories

--- user_journeys/dev/cfg/ctt.py
+++ user_journeys/dev/cfg/ctt.py
@@ -1,4 +1,3 @@
-# ctt.py
 import cfg.sparql as sparql
 
 CELLAR_CONNECTION = "http://publications.europa.eu/webapi/rdf/sparql"
@@ -7,43 +6,24 @@ URL_MICROSERVICE = 'http://localhost:5100/assessments/get_assessments'
 
 PROCURER_USE_CASES = [
     {'name': 'UC-1',
-     'title': 'SPECIFICATIONS CATALOG',
+     'title': '<strong>SPECIFICATIONS CATALOG</strong>',
      'query_value': sparql.QUERY_ALL_SPECIFICATIONS_ASSESSED,
      'description': '''This SPARQL query returns the interoperability specifications catalog by CAMSS and their 
-      assessments. Press the Run button to execute it. You can also modify the query if you need to complement it'''},
-    {'name': 'UC-2',
-     'title': 'ONE SPECIFICATION ASSESSMENT',
-     'query_value': '',
-     'description': '''This SPARQL query re-directs the user to one Specification Assessment dataset and report. Press the Run button to execute it. You can also 
-     modify the query if you need to complement it'''},
-    {'name': 'UC-3',
-     'title': '',
-     'query_value': '',
-     'description': '''Downloads one concrete scenario-based CAMSS Assessments. Press the Run button to execute it. You can also modify the query 
-     if you need to complement it'''},
+      assessments. You can also modify the query if you need to complement it.<br>Press the Run button to execute it.'''},
     {'name': "Custom queries",
-     'title': '',
+     'title': '<strong>CUSTOM QUERIES</strong>',
      'query_value': '',
-     'description': '''In this section you can develop a SPARQL query of your own. Type the query in the textarea and press the Run button to
+     'description': '''In this section you can develop a SPARQL query of your own.<br>Type the query in the textarea and press the Run button to
      execute it.'''}]
 
 SOFTWARE_USE_CASES = [
     {'name': 'UC-1',
-     'title': 'SPECIFICATIONS ASSOCIATED TO EIRA ABBs',
+     'title': '<strong>SPECIFICATIONS ASSOCIATED TO EIRA ABBs</strong>',
      'query_value': sparql.QUERY_SPECIFICATIONS_ASSESSED_ASSOCIATED_TO_EIRA_ABBS,
      'description': '''This SPARQL query returns the interoperability specifications assessed by CAMSS and associated to EIRA ABBs.
-     Press the Run button to execute it. You can also modify the query if you need to complement it'''},
-    {'name': 'UC-2',
-     'title': 'ONE SPECIFICATION ASSESSMENT',
-     'query_value': '',
-     'description': '''This SPARQL query re-directs the user to one Specification Assessment dataset and report. Press the Run button to execute it. You can also 
-     modify the query if you need to complement it'''},
-    {'name': 'UC-3',
-     'title': '',
-     'query_value': '',
-     'description': '''...'''},
+     You can also modify the query if you need to complement it.<br>Press the Run button to execute it.'''},
     {'name': "Custom queries",
-     'title': '',
+     'title': '<strong>CUSTOM QUERIES</strong>',
      'query_value': '',
-     'description': '''In this section you can develop a SPARQL query of your own. Type the query in the textarea and press the Run button to
+     'description': '''In this section you can develop a SPARQL query of your own.<br>Type the query in the textarea and press the Run button to
      execute it.'''}]
